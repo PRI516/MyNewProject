a909967419ed549af02a35581b0165a95960db41
Priyanka Tudu <167540256+PRI516@users.noreply.github.com>
2025-09-04 07:59:37 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const highlightBtn = document.getElementById("highlightBtn");
+
+highlightBtn.addEventListener("click", () => {
+  // Open CodeSandbox in a new tab
+  window.open("https://codesandbox.io/", "_blank");
+});
